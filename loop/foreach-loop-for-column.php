f5d91c32a1a57685655999bb5682ac6a7493b159
Sakib Ahmed <msakibahmed3s@gmail.com>
2018-08-08 10:57:09 +0600

    foreach loop for column

--- loop/foreach-loop-for-column.php
+++ loop/foreach-loop-for-column.php
@@ -1,24 +1,21 @@
 
-<?php 
-
-global $wpdb;
-	$results = $wpdb->get_results( "SELECT * FROM countries ORDER BY county" );									
-	$rowcount = $wpdb->num_rows;
-	//$divid  	= $rowcount/7+1;
-	$divid  	= $rowcount/8;
-	$per_column = round($divid);
-	
-	 <?php 
-	foreach($results as $kicks) {
-	echo '<div class="col-sm-4 col-md-3">';
+<?php
+	//Columns must be a factor of 12 (1,2,3,4,6,12)
+	$numOfCols = 2;
+	$rowCount = 0;
+	$bootstrapColWidth = 12 / $numOfCols;
 	?>
-		<div class="single-countylist">
-			<a href="/county/?county-name=<?php echo($kicks->county);?>">
-				<img src="<?php if(!empty($kicks->flag)){echo 'data:image/jpeg;base64, '.$kicks->flag;}else{echo get_template_directory_uri().'/img/wf.png';} ?>" alt="" >
-				<div class="all-county-name"><?php echo($kicks->county);?></div>
-			</a>
-		</div>
+	
 	<?php
-	echo '</div>';
+	foreach ($course_details as $row){
+	?>  
+	        <div class="col-lg-6 col-12 col-sm-6 <?php //echo $bootstrapColWidth; ?>">
+	            <ul class="list-inline">
+	                <li><?php echo $row['item_name']; ?></li>
+	            </ul>
+	        </div>
+	<?php
+	    $rowCount++;
+	    if($rowCount % $numOfCols == 0) ;
 	}
 	?>
